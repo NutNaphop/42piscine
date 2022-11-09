e87952896e5b817a78758f0c225781a6b58a8f22
nkhumcha <nkhumcha@DESKTOP-SJ6PTBI.localdomain>
2022-11-09 15:44:12 +0700

    C00

---
+++ C00/ex06/ft_print_comb2.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkhumcha <nkhumcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 15:36:58 by nkhumcha          #+#    #+#             */
+/*   Updated: 2022/11/09 15:38:50 by nkhumcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include<unistd.h>
+
+void	ft_putchar(char a)
+{
+	write(1, &a, 1);
+}
+
+void	ft_print_comb2(void)
+{
+	char	first;
+	char	second;
+
+	first = -1;
+	while (first++ < 98)
+	{
+		second = first;
+		while (second++ < 99)
+		{
+			ft_putchar(first / 10 + '0');
+			ft_putchar(first % 10 + '0');
+			ft_putchar(' ');
+			ft_putchar(second / 10 + '0');
+			ft_putchar(second % 10 + '0');
+			if (first != 98)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+		}
+	}
+}
+
+int	main(void)
+{
+	ft_print_comb2();
+}
