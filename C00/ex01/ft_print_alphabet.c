2af2244f3d316066aefb62a325011d9cedcfdb2e
nkhumcha <nkhumcha@DESKTOP-SJ6PTBI.localdomain>
2022-11-09 12:39:40 +0700

    C00

---
+++ C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nkhumcha <nkhumcha@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 12:06:55 by nkhumcha          #+#    #+#             */
+/*   Updated: 2022/11/09 12:38:40 by nkhumcha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_alphabet(void)
+{
+	char	alpha;
+
+	alpha = 'a';
+	while (alpha <= 'z')
+	{
+		ft_print_putchar(alpha);
+		alpha ++;
+	}
+}
+
+int	main(void)
+{
+	ft_print_alphabet();
+}
